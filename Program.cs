bc2ca0578b6f44799a716bf0047798886c1bb900
Joshua Hooper <joshuahooper3@gmail.com>
2021-01-25 23:46:46 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace bmrCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the BMR (Basal Metabolic Rate) calculator!");
+
+
+            Console.WriteLine("\nEnter your weight in kilograms: ");
+
+
+            Console.WriteLine("Enter your height in centimeters: ");
+
+
+            Console.WriteLine("Enter your age: ");
+
+
+            Console.WriteLine("Enter M for male or F for female: ");
+
+
+
+        }
+    }
+}
